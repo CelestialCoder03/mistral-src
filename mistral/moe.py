9d71f3797c34cb4422f4dd2aff826a0e5e76378c
CelestialCoder <49389909+CelestialCoder03@users.noreply.github.com>
2024-01-12 10:49:38 +0800

    Update docstring

--- mistral/moe.py
+++ mistral/moe.py
@@ -23,10 +23,12 @@ class MoeLayer(nn.Module):
 
     def forward(self, inputs: torch.Tensor):
         gate_logits = self.gate(inputs)
+        # logits is 2D
         weights, selected_experts = torch.topk(gate_logits, self.args.num_experts_per_tok)
         weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
         results = torch.zeros_like(inputs)
         for i, expert in enumerate(self.experts):
+            # selected experts is 2D
             batch_idx, nth_expert = torch.where(selected_experts == i)
             results[batch_idx] += weights[batch_idx, nth_expert, None] * expert(
                 inputs[batch_idx]
